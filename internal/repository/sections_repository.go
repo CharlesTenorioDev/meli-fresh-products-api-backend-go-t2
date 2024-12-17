3c3a33499c4bfed112bbf25fda2c73008b2dd00b
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2024-12-17 16:01:44 -0300

    testing sections in memory repository

--- internal/repository/sections_repository.go
+++ internal/repository/sections_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"cmp"
 	"slices"
 
 	"github.com/meli-fresh-products-api-backend-go-t2/internal/pkg"
@@ -12,31 +13,38 @@ type MemorySectionRepository struct {
 	nextId int
 }
 
-func NewMemorySectionRepository() pkg.SectionRepository {
+// Initialize a MemorySectionRepository and set the next id to 1
+func NewMemorySectionRepository(load map[int]pkg.Section) pkg.SectionRepository {
 	repo := &MemorySectionRepository{}
-	repo.db = make(map[int]pkg.Section)
-	repo.nextId = 1
+	if len(load) == 0 {
+		repo.db = make(map[int]pkg.Section)
+		repo.nextId = 1
+	} else {
+		repo.db = load
+		repo.nextId = utils.GetBiggestId(load) + 1
+	}
 	return repo
 }
 
+// Get all the sections and return in asc order
 func (r MemorySectionRepository) GetAll() ([]pkg.Section, error) {
 	sections := []pkg.Section{}
 	for _, section := range r.db {
 		sections = append(sections, section)
 	}
 	slices.SortFunc(sections, func(a pkg.Section, b pkg.Section) int {
-		if a.ID > b.ID {
-			return 0
-		}
-		return 1
+		return cmp.Compare(a.ID, b.ID)
 	})
 	return sections, nil
 }
 
+// Return the r.db[id]
 func (r *MemorySectionRepository) GetById(id int) (pkg.Section, error) {
 	return r.db[id], nil
 }
 
+// Finds the section by its sectionNumber
+// If not found, pkg.Section{} is returned
 func (r *MemorySectionRepository) GetBySectionNumber(sectionNumber int) (pkg.Section, error) {
 	for _, section := range r.db {
 		if section.SectionNumber == sectionNumber {
@@ -46,6 +54,8 @@ func (r *MemorySectionRepository) GetBySectionNumber(sectionNumber int) (pkg.Sec
 	return pkg.Section{}, nil
 }
 
+// Generate a new ID and save the entity
+// All validatinos should be made on service layer
 func (r *MemorySectionRepository) Save(newSection pkg.Section) (pkg.Section, error) {
 	newSection.ID = r.nextId
 	r.nextId++
@@ -53,11 +63,15 @@ func (r *MemorySectionRepository) Save(newSection pkg.Section) (pkg.Section, err
 	return r.db[newSection.ID], nil
 }
 
+// Update the map
+// If a section does not exist for the id, a new one will be created
 func (r *MemorySectionRepository) Update(section pkg.Section) (pkg.Section, error) {
 	r.db[section.ID] = section
 	return r.db[section.ID], nil
 }
 
+// Delete the section by its id
+// If no sections exists by id attribute, utils.ErrNotFound is returned
 func (r *MemorySectionRepository) Delete(id int) error {
 	if r.db[id] == (pkg.Section{}) {
 		return utils.ErrNotFound
