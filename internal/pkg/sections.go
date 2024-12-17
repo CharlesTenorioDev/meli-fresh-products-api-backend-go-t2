b5352a7886e5ed5daa6e294f2ba5949de3a01a89
watanabe9090 <yuriwatanabe9090.@gmail.com>
2024-12-17 13:45:19 -0300

    feat: start sections

---
+++ internal/pkg/sections.go
@@ -0,0 +1,44 @@
+package pkg
+
+type Section struct {
+	ID                 int
+	SectionNumber      int
+	CurrentCapacity    int
+	MaximumCapacity    int
+	MinimumCapacity    int
+	CurrentTemperature float64
+	MinimumTemperature float64
+	ProductTypeID      int
+	WarehouseID        int
+}
+
+type SectionPointers struct {
+	ID                 *int
+	SectionNumber      *int
+	CurrentCapacity    *int
+	MaximumCapacity    *int
+	MinimumCapacity    *int
+	CurrentTemperature *float64
+	MinimumTemperature *float64
+	ProductTypeID      *int
+	WarehouseID        *int
+}
+
+type (
+	SectionRepository interface {
+		GetAll() ([]Section, error)
+		Save(Section) (Section, error)
+		Update(Section) (Section, error)
+		GetById(int) (Section, error)
+		GetBySectionNumber(int) (Section, error)
+		Delete(int) error
+	}
+	SectionService interface {
+		GetAll() ([]Section, error)
+		Save(Section) (Section, error)
+		Update(int, Section) (Section, error)
+		UpdateByFields(int, SectionPointers) (Section, error)
+		GetById(int) (Section, error)
+		Delete(int) error
+	}
+)
