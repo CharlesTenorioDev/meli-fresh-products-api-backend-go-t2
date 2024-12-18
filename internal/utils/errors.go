ebd8bc380b5b42707c1befa0e9b6212366923c76
Barbara Teresa Toledo <barbara-teresa@outlook.com>
2024-12-18 12:16:47 -0300

    chore: update utils

--- internal/utils/errors.go
+++ internal/utils/errors.go
@@ -5,10 +5,10 @@ import (
 )
 
 var (
-	ErrInvalidFormat     = errors.New("invalid format")        // 400
-	ErrInvalidArguments  = errors.New("invalid arguments")     // 422
-	ErrConflict          = errors.New("entity already exists") // 409
-	ErrNotFound          = errors.New("entity not found")      // 404
-	ErrInvalidProperties = errors.New("invalid properties format")
+	ErrInvalidFormat     = errors.New("invalid format")              // 400
+	ErrInvalidArguments  = errors.New("invalid arguments")           // 422
+	ErrConflict          = errors.New("entity already exists")       // 409
+	ErrNotFound          = errors.New("entity not found")            // 404
+	ErrInvalidProperties = errors.New("invalid properties format")   // For parsing the properties, panic
 	ErrEmptyArguments    = errors.New("arguments must not be empty") // 400
 )
--- internal/utils/ids.go
+++ internal/utils/ids.go
@@ -1,13 +1,16 @@
 package utils
 
-func GetBiggestId[T any](db map[int]T) int {
+// Returns the biggest id of a int map
+// if len(map) == 0, returns 1
+func GetBiggestId[K int, V any](db map[K]V) int {
 	if len(db) == 0 {
 		return 1
 	}
 	biggest := -1
 	for key := range db {
-		if key > biggest {
-			biggest = key
+		k2 := int(key)
+		if k2 > biggest {
+			biggest = k2
 		}
 	}
 	return biggest
