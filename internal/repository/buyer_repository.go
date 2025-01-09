5807f13466aabcbc396b8051fa6cc4e32aa64fab
Barbara Teresa Toledo <barbara-teresa@outlook.com>
2025-01-08 23:42:46 -0300

    feat: starting to add mysql

--- internal/repository/buyer_repository.go
+++ internal/repository/buyer_repository.go
@@ -1,178 +1,93 @@
 package repository
 
 import (
-	"encoding/json"
-	"github.com/meli-fresh-products-api-backend-go-t2/internal"
-	"log"
-	"os"
+	"database/sql"
+	"errors"
 
+	"github.com/meli-fresh-products-api-backend-go-t2/internal"
 	"github.com/meli-fresh-products-api-backend-go-t2/internal/utils"
 )
 
 type BuyerRepo struct {
-	buyerTable map[int]internal.Buyer
+	db *sql.DB
 }
 
-func NewBuyerDb(buyerTab map[int]internal.Buyer) *BuyerRepo {
-
-	BuyerDb := make(map[int]internal.Buyer)
-	if buyerTab != nil {
-		BuyerDb = buyerTab
-	}
-	return &BuyerRepo{buyerTable: BuyerDb}
-}
-
-var buyersFile = "./docs/db/buyers.json"
-
-func (repo *BuyerRepo) LoadBuyers() (map[int]internal.Buyer, error) {
-	file, err := os.ReadFile(buyersFile)
-	if err != nil {
-		log.Println("Error to read file", err)
-		return nil, err
-	}
-
-	var buyers []internal.Buyer
-
-	if err := json.Unmarshal(file, &buyers); err != nil {
-		log.Println("Error to unmarshal - ")
-		return nil, err
-	}
-
-	repo.buyerTable = make(map[int]internal.Buyer)
-	for _, buyer := range buyers {
-		repo.buyerTable[int(buyer.ID)] = buyer
-	}
-
-	return repo.buyerTable, nil
+func NewBuyerDb(db *sql.DB) *BuyerRepo {
+	return &BuyerRepo{db}
 }
 
 func (repo *BuyerRepo) GetAll() ([]internal.Buyer, error) {
-	buyersMap, err := repo.LoadBuyers()
+	query := "SELECT id, id_card_number, first_name, last_name FROM buyers"
+	rows, err := repo.db.Query(query)
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	var buyers []internal.Buyer
-	for _, buyer := range buyersMap {
-		buyers = append(buyers, buyer)
+	for rows.Next() {
+		var b internal.Buyer
+		err := rows.Scan(&b.ID, &b.CardNumberID, &b.FirstName, &b.LastName)
+		if err != nil {
+			return nil, err
+		}
+		buyers = append(buyers, b)
 	}
-
-	return buyers, nil
+	return buyers, rows.Err()
 }
 
 func (repo *BuyerRepo) GetOne(id int) (*internal.Buyer, error) {
-	buyersMap, err := repo.LoadBuyers()
+	query := "SELECT id, id_card_number, first_name, last_name FROM buyers WHERE id = ?"
+	row := repo.db.QueryRow(query, id)
+
+	var buyer internal.Buyer
+	err := row.Scan(&buyer.ID, &buyer.CardNumberID, &buyer.FirstName, &buyer.LastName)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, utils.ErrNotFound
+	}
 	if err != nil {
-		log.Println("Error to Load Buyers - ", err)
 		return nil, err
 	}
-
-	if buyer, exists := buyersMap[id]; exists {
-		return &buyer, err
-	}
-	return nil, err
+	return &buyer, nil
 }
 
 func (repo *BuyerRepo) CreateBuyer(newBuyer internal.Buyer) (*internal.Buyer, error) {
-	buyers, err := repo.GetAll()
-
+	query := "INSERT INTO buyers (id_card_number, first_name, last_name) VALUES (?, ?, ?)"
+	result, err := repo.db.Exec(query, newBuyer.CardNumberID, newBuyer.FirstName, newBuyer.LastName)
 	if err != nil {
-		log.Println("Error to load - ", err)
 		return nil, err
 	}
 
-	for _, buyer := range buyers {
-		if buyer.ID == newBuyer.ID {
-			log.Println("There is an user with this ID", err)
-			return nil, utils.ErrConflict
-		}
-	}
-	buyers = append(buyers, newBuyer)
-
-	file, err := os.Create(buyersFile)
+	insertedID, err := result.LastInsertId()
 	if err != nil {
-		log.Println("Erro ao reabrir o arquivo:", err)
 		return nil, err
 	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(buyers); err != nil {
-		log.Println("Erro ao codificar JSON:", err)
-		return nil, err
-	}
-	log.Println("Comprador salvo!")
-
+	newBuyer.ID = int64(insertedID)
 	return &newBuyer, nil
 }
 
 func (repo *BuyerRepo) UpdateBuyer(updatedBuyer *internal.Buyer) (*internal.Buyer, error) {
-	buyers, err := repo.GetAll()
-
+	query := "UPDATE buyers SET id_card_number = ?, first_name = ?, last_name = ? WHERE id = ?"
+	_, err := repo.db.Exec(query, updatedBuyer.CardNumberID, updatedBuyer.FirstName, updatedBuyer.LastName, updatedBuyer.ID)
 	if err != nil {
-		log.Println("Error to load - ", err)
 		return nil, err
 	}
-
-	for i, buyer := range buyers {
-		if buyer.ID == updatedBuyer.ID {
-			buyers[i] = *updatedBuyer
-		}
-	}
-
-	file, err := os.Create(buyersFile)
-
-	if err != nil {
-		log.Println("Error to open file:", err)
-		return nil, err
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(buyers); err != nil {
-		log.Println("Error to encoder JSON:", err)
-		return nil, err
-	}
-	log.Println("Comprador atualizado!")
-
 	return updatedBuyer, nil
 }
 
 func (repo *BuyerRepo) DeleteBuyer(id int) error {
-	buyers, err := repo.GetAll()
+	query := "DELETE FROM buyers WHERE id = ?"
+	result, err := repo.db.Exec(query, id)
 	if err != nil {
-		log.Println("Erro ao carregar os compradores:", err)
 		return err
 	}
 
-	indexToDelete := -1
-	for i, buyer := range buyers {
-		if int(buyer.ID) == id {
-			indexToDelete = i
-			break
-		}
-	}
-
-	if indexToDelete == -1 {
-		log.Println("Comprador com o ID fornecido não encontrado")
-		return utils.ErrInvalidArguments
-	}
-
-	buyers = append(buyers[:indexToDelete], buyers[indexToDelete+1:]...)
-
-	file, err := os.Create(buyersFile)
+	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		log.Println("Erro ao abrir o arquivo para escrita:", err)
 		return err
 	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(buyers); err != nil {
-		log.Println("Erro ao codificar JSON:", err)
-		return err
+	if rowsAffected == 0 {
+		return utils.ErrNotFound
 	}
-
-	log.Println("Comprador excluído com sucesso!")
 	return nil
 }
