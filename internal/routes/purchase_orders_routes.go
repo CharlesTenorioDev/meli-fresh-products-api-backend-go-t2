7bface372d4b2a95e77f7f0444e493cb8636f74a
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2025-01-14 09:40:20 -0300

    fix report route

--- internal/routes/purchase_orders_routes.go
+++ internal/routes/purchase_orders_routes.go
@@ -9,12 +9,11 @@ import (
 // RegisterPurchaseOrdersRoutes is used to record the routes associated to the purchase orders entity
 func RegisterPurchaseOrdersRoutes(mux *chi.Mux, service internal.PurchaseOrderService) error {
 	handler := handler.NewPurchaseOrdersHandler(service)
-	mux.Route("/api/v1/PurchaseOrders", func(router chi.Router) {
-		// Get
-		router.Get("/", handler.GetAllPurchaseOrders())
+	mux.Route("/api/v1/purchaseOrders", func(router chi.Router) {
 		// Post
 		router.Post("/", handler.PostPurchaseOrders())
 	})
+	mux.HandleFunc("/api/v1/buyers/reportPurchaseOrders", handler.GetAllPurchaseOrders())
 
 	return nil
 }
