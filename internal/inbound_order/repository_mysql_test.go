4464d4e5f2f7c562bca1f7323599034dd52dcba2
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2025-02-17 15:21:26 -0300

    adapting inbound_orders tests to use testcontainers

--- internal/inbound_order/repository_mysql_test.go
+++ internal/inbound_order/repository_mysql_test.go
@@ -4,308 +4,232 @@ import (
 	"database/sql"
 	"errors"
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/DATA-DOG/go-txdb"
-	"github.com/go-sql-driver/mysql"
 	"github.com/meli-fresh-products-api-backend-go-t2/internal"
+	"github.com/meli-fresh-products-api-backend-go-t2/internal/testutils"
 	"github.com/meli-fresh-products-api-backend-go-t2/internal/utils"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func init() {
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "example",
-		Net:    "tcp",
-		Addr:   "localhost:3307",
-		DBName: "fresh_products",
-	}
-	txdb.Register("txdb", "mysql", cfg.FormatDSN())
-}
+func TestUnitInboundOrderRepository(t *testing.T) {
+	t.Run("GenerateInboundOrdersReport", func(t *testing.T) {
+		t.Run("FAIL Generate Inbound Orders Report", func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
 
-func setupTestDB(t *testing.T) *sql.DB {
-	db, err := sql.Open("txdb", "identifier")
-	if err != nil {
-		t.Fatalf("failed to open db: %v", err)
-	}
-	return db
-}
+			mock.ExpectQuery("SELECT e.id, e.id_card_number, e.first_name, e.last_name, e.warehouse_id, COUNT(o.id) as inbound_orders_count").
+				WillReturnError(sql.ErrConnDone)
 
-func TestNewMySqlInboundOrderRepository(t *testing.T) {
-	db := setupTestDB(t)
-	defer db.Close()
+			repo := NewMySqlInboundOrderRepository(db)
 
-	repo := NewMySqlInboundOrderRepository(db)
-	require.NotNil(t, repo)
-}
+			result, err := repo.GenerateInboundOrdersReport()
+			require.Error(t, err)
+			require.Empty(t, result)
+		})
 
-func TestCreateInboundOrder(t *testing.T) {
-	t.Run("SUCCESS Create Inbound Order", func(t *testing.T) {
-		var (
-			newOrder = internal.InboundOrderAttributes{
-				OrderDate:      "2023-10-01",
-				OrderNumber:    "12345",
-				EmployeeID:     1,
-				ProductBatchID: 1,
-				WarehouseID:    1,
+	})
+
+	t.Run("GenerateByIDInboundOrdersReport", func(t *testing.T) {
+
+		t.Run("FAIL Generate Inbound Orders Report by ID", func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
+
+			employeeID := 1
+
+			mock.ExpectQuery("").
+				WillReturnError(sql.ErrNoRows)
+
+			repo := NewMySqlInboundOrderRepository(db)
+
+			result, err := repo.GenerateByIDInboundOrdersReport(employeeID)
+
+			if errors.Is(err, sql.ErrNoRows) {
+				err = utils.ErrNotFound
 			}
-		)
-		db := setupTestDB(t)
-		defer db.Close()
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		order, err := repo.CreateInboundOrder(newOrder)
-		require.NoError(t, err)
-		require.NotZero(t, order.ID)
-		require.Equal(t, newOrder.OrderNumber, order.Attributes.OrderNumber)
+			require.Error(t, err)
+			require.Empty(t, result)
+			require.Equal(t, utils.ErrNotFound, err)
+		})
 	})
 
-	t.Run("FAIL Create Inbound Order", func(t *testing.T) {
-		var (
-			newOrder       = internal.InboundOrderAttributes{}
-			wantErr  error = nil
-		)
-		db := setupTestDB(t)
-		defer db.Close()
+	t.Run("FindByID", func(t *testing.T) {
+		t.Run("FAIL Find Inbound Order by ID (Error Not Found)", func(t *testing.T) {
+			var (
+				expectedOrder = internal.InboundOrder{}
+				id            = 1
+			)
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
 
-		repo := NewMySqlInboundOrderRepository(db)
-		order, err := repo.CreateInboundOrder(newOrder)
-		require.Error(t, err)
-		require.Empty(t, order.ID)
-		require.NotEqual(t, nil, order)
-		require.NotEqual(t, wantErr, err)
-	})
-}
+			mock.ExpectQuery("").
+				WillReturnError(sql.ErrNoRows)
 
-func TestMysqlInboundOrderRepository_GenerateInboundOrdersReport(t *testing.T) {
-	t.Run("SUCCESS Generate Inbound Orders Report", func(t *testing.T) {
-		var (
-			expectedReport = []internal.EmployeeInboundOrdersReport{
-				{
-					ID:                 1,
-					CardNumberID:       "123456",
-					FirstName:          "John",
-					LastName:           "Doe",
-					WarehouseID:        1,
-					InboundOrdersCount: 0,
-				},
-				{
-					ID:                 2,
-					CardNumberID:       "654321",
-					FirstName:          "Jane",
-					LastName:           "Smith",
-					WarehouseID:        2,
-					InboundOrdersCount: 1,
-				},
+			repo := NewMySqlInboundOrderRepository(db)
+
+			result, err := repo.FindByID(id)
+			if errors.Is(err, sql.ErrNoRows) {
+				err = utils.ErrNotFound
 			}
-		)
 
-		db := setupTestDB(t)
-		defer db.Close()
+			require.Error(t, err)
+			require.Empty(t, result)
+			require.Equal(t, utils.ErrNotFound, err)
+			require.Equal(t, expectedOrder, result)
+		})
 
-		repo := NewMySqlInboundOrderRepository(db)
+		t.Run("FAIL Find Inbound Order by ID (Any other error)", func(t *testing.T) {
+			var (
+				expectedOrder = internal.InboundOrder{}
+				id            = 1
+			)
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
 
-		result, err := repo.GenerateInboundOrdersReport()
-		require.NoError(t, err)
-		require.NotEmpty(t, result)
-		require.Equal(t, expectedReport, result)
-	})
+			mock.ExpectQuery("").
+				WillReturnError(sql.ErrConnDone)
 
-	t.Run("FAIL Generate Inbound Orders Report", func(t *testing.T) {
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
-		defer db.Close()
+			repo := NewMySqlInboundOrderRepository(db)
 
-		mock.ExpectQuery("SELECT e.id, e.id_card_number, e.first_name, e.last_name, e.warehouse_id, COUNT(o.id) as inbound_orders_count").
-			WillReturnError(sql.ErrConnDone)
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.GenerateInboundOrdersReport()
-		require.Error(t, err)
-		require.Empty(t, result)
-	})
-
-}
-
-func TestMysqlInboundOrderRepository_GenerateByIDInboundOrdersReport(t *testing.T) {
-	t.Run("SUCCESS Generate Inbound Orders Report by ID", func(t *testing.T) {
-		var (
-			expectedReport = internal.EmployeeInboundOrdersReport{
-				ID:                 1,
-				CardNumberID:       "123456",
-				FirstName:          "John",
-				LastName:           "Doe",
-				WarehouseID:        1,
-				InboundOrdersCount: 0,
+			result, err := repo.FindByID(id)
+			if errors.Is(err, sql.ErrNoRows) {
+				err = utils.ErrNotFound
 			}
-			employeeId = 1
-		)
 
-		db := setupTestDB(t)
-		defer db.Close()
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.GenerateByIDInboundOrdersReport(employeeId)
-		require.NoError(t, err)
-		require.NotEmpty(t, result)
-		require.Equal(t, expectedReport, result)
+			require.Error(t, err)
+			require.Empty(t, result)
+			require.Equal(t, sql.ErrConnDone, err)
+			require.Equal(t, expectedOrder, result)
+		})
 	})
 
-	t.Run("FAIL Generate Inbound Orders Report by ID", func(t *testing.T) {
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
-		defer db.Close()
+	t.Run("FindByOrderNumber", func(t *testing.T) {
+		t.Run("FAIL Find Inbound Order by Order Number", func(t *testing.T) {
+			var (
+				wantInboundOrder = internal.InboundOrder{}
+				orderNumber      = "order#1"
+			)
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
 
-		employeeID := 1
+			mock.ExpectQuery("").
+				WillReturnError(sql.ErrNoRows)
 
-		mock.ExpectQuery("").
-			WillReturnError(sql.ErrNoRows)
+			repo := NewMySqlInboundOrderRepository(db)
 
-		repo := NewMySqlInboundOrderRepository(db)
+			result, err := repo.FindByOrderNumber(orderNumber)
+			if errors.Is(err, sql.ErrNoRows) {
+				err = utils.ErrNotFound
+			}
 
-		result, err := repo.GenerateByIDInboundOrdersReport(employeeID)
-
-		if errors.Is(err, sql.ErrNoRows) {
-			err = utils.ErrNotFound
-		}
-		require.Error(t, err)
-		require.Empty(t, result)
-		require.Equal(t, utils.ErrNotFound, err)
+			require.Error(t, err)
+			require.Empty(t, result)
+			require.Equal(t, utils.ErrNotFound, err)
+			require.Equal(t, wantInboundOrder, result)
+		})
 	})
 }
 
-func TestMysqlInboundOrderRepository_FindByID(t *testing.T) {
-	t.Run("SUCCESS Find Inbound Order by ID", func(t *testing.T) {
-		var (
-			expectedOrder = internal.InboundOrder{
-				ID: 1,
-				Attributes: internal.InboundOrderAttributes{
-					OrderDate:      "2021-04-04 00:00:00.000000",
-					OrderNumber:    "order#1",
-					EmployeeID:     2,
+func TestIntegrationInboundOrderRepository(t *testing.T) {
+	ts := testutils.GetTestDBConn()
+	defer ts.End()
+	repo := NewMySqlInboundOrderRepository(ts.DB)
+
+	t.Run("NewMySqlInboundOrderRepository", func(t *testing.T) {
+		require.NotNil(t, repo)
+	})
+
+	t.Run("Create", func(t *testing.T) {
+		t.Run("SUCCESS Create Inbound Order", func(t *testing.T) {
+			var (
+				newOrder = internal.InboundOrderAttributes{
+					OrderDate:      "2023-10-01",
+					OrderNumber:    "12345",
+					EmployeeID:     1,
 					ProductBatchID: 1,
 					WarehouseID:    1,
-				},
-			}
-			id = 1
-		)
+				}
+			)
+			order, err := repo.CreateInboundOrder(newOrder)
+			require.NoError(t, err)
+			require.NotZero(t, order.ID)
+			require.Equal(t, newOrder.OrderNumber, order.Attributes.OrderNumber)
+		})
 
-		db := setupTestDB(t)
-		defer db.Close()
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.FindByID(id)
-
-		require.NoError(t, err)
-		require.NotEmpty(t, result)
-		require.Equal(t, expectedOrder, result)
+		t.Run("FAIL Create Inbound Order", func(t *testing.T) {
+			var (
+				newOrder       = internal.InboundOrderAttributes{}
+				wantErr  error = nil
+			)
+			order, err := repo.CreateInboundOrder(newOrder)
+			require.Error(t, err)
+			require.Empty(t, order.ID)
+			require.NotEqual(t, nil, order)
+			require.NotEqual(t, wantErr, err)
+		})
 	})
 
-	t.Run("FAIL Find Inbound Order by ID (Error Not Found)", func(t *testing.T) {
-		var (
-			expectedOrder = internal.InboundOrder{}
-			id            = 1
-		)
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
-		defer db.Close()
+	t.Run("GenerateInboundOrdersReport", func(t *testing.T) {
+		t.Run("SUCCESS Generate Inbound Orders Report", func(t *testing.T) {
+			var (
+				expectedReport = []internal.EmployeeInboundOrdersReport{
+					{ID: 1, CardNumberID: "E001", FirstName: "Alice", LastName: "Johnson", WarehouseID: 1, InboundOrdersCount: 2},
+					{ID: 2, CardNumberID: "E002", FirstName: "Bob", LastName: "Smith", WarehouseID: 2, InboundOrdersCount: 1}}
+			)
 
-		mock.ExpectQuery("").
-			WillReturnError(sql.ErrNoRows)
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.FindByID(id)
-		if errors.Is(err, sql.ErrNoRows) {
-			err = utils.ErrNotFound
-		}
-
-		require.Error(t, err)
-		require.Empty(t, result)
-		require.Equal(t, utils.ErrNotFound, err)
-		require.Equal(t, expectedOrder, result)
+			result, err := repo.GenerateInboundOrdersReport()
+			require.NoError(t, err)
+			require.NotEmpty(t, result)
+			require.Equal(t, expectedReport, result)
+		})
 	})
 
-	t.Run("FAIL Find Inbound Order by ID (Any other error)", func(t *testing.T) {
-		var (
-			expectedOrder = internal.InboundOrder{}
-			id            = 1
-		)
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
-		defer db.Close()
-
-		mock.ExpectQuery("").
-			WillReturnError(sql.ErrConnDone)
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.FindByID(id)
-		if errors.Is(err, sql.ErrNoRows) {
-			err = utils.ErrNotFound
-		}
-
-		require.Error(t, err)
-		require.Empty(t, result)
-		require.Equal(t, sql.ErrConnDone, err)
-		require.Equal(t, expectedOrder, result)
-	})
-}
-
-func TestMysqlInboundOrderRepository_FindByOrderNumber(t *testing.T) {
-	t.Run("SUCCESS Find Inbound Order by Order Number", func(t *testing.T) {
-		var (
-			expectedOrder = internal.InboundOrder{
-				ID: 1,
-				Attributes: internal.InboundOrderAttributes{
-					OrderDate:      "2021-04-04 00:00:00.000000",
-					OrderNumber:    "order#1",
-					EmployeeID:     2,
-					ProductBatchID: 1,
-					WarehouseID:    1,
-				},
-			}
-			orderNumber = "order#1"
-		)
-
-		db := setupTestDB(t)
-		defer db.Close()
-
-		repo := NewMySqlInboundOrderRepository(db)
-
-		result, err := repo.FindByOrderNumber(orderNumber)
-
-		require.NoError(t, err)
-		require.NotEmpty(t, result)
-		require.Equal(t, expectedOrder, result)
+	t.Run("GenerateByIDInboundOrdersReport", func(t *testing.T) {
+		t.Run("SUCCESS Generate Inbound Orders Report by ID", func(t *testing.T) {
+			var (
+				expectedReport = internal.EmployeeInboundOrdersReport{ID: 1, CardNumberID: "E001", FirstName: "Alice", LastName: "Johnson", WarehouseID: 1, InboundOrdersCount: 2}
+				employeeId     = 1
+			)
+			result, err := repo.GenerateByIDInboundOrdersReport(employeeId)
+			require.NoError(t, err)
+			require.NotEmpty(t, result)
+			require.Equal(t, expectedReport, result)
+		})
 	})
 
-	t.Run("FAIL Find Inbound Order by Order Number", func(t *testing.T) {
-		var (
-			wantInboundOrder = internal.InboundOrder{}
-			orderNumber      = "order#1"
-		)
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
-		defer db.Close()
+	t.Run("FindByID", func(t *testing.T) {
+		t.Run("SUCCESS Find Inbound Order by ID", func(t *testing.T) {
+			var (
+				expectedOrder = internal.InboundOrder{
+					ID:         1,
+					Attributes: internal.InboundOrderAttributes{OrderDate: "2025-01-05 12:00:00.000000", OrderNumber: "IN001", EmployeeID: 1, ProductBatchID: 1, WarehouseID: 1},
+				}
+				id = 1
+			)
+			result, err := repo.FindByID(id)
 
-		mock.ExpectQuery("").
-			WillReturnError(sql.ErrNoRows)
+			require.NoError(t, err)
+			require.NotEmpty(t, result)
+			require.Equal(t, expectedOrder, result)
+		})
+	})
 
-		repo := NewMySqlInboundOrderRepository(db)
+	t.Run("FindByOrderNumber", func(t *testing.T) {
+		t.Run("SUCCESS Find Inbound Order by Order Number", func(t *testing.T) {
+			var (
+				expectedOrder = internal.InboundOrder{ID: 1, Attributes: internal.InboundOrderAttributes{OrderDate: "2025-01-05 12:00:00.000000", OrderNumber: "IN001", EmployeeID: 1, ProductBatchID: 1, WarehouseID: 1}}
+				orderNumber   = "IN001"
+			)
+			result, err := repo.FindByOrderNumber(orderNumber)
 
-		result, err := repo.FindByOrderNumber(orderNumber)
-		if errors.Is(err, sql.ErrNoRows) {
-			err = utils.ErrNotFound
-		}
-
-		require.Error(t, err)
-		require.Empty(t, result)
-		require.Equal(t, utils.ErrNotFound, err)
-		require.Equal(t, wantInboundOrder, result)
+			require.NoError(t, err)
+			require.NotEmpty(t, result)
+			require.Equal(t, expectedOrder, result)
+		})
 	})
 }
